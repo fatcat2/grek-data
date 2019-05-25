8ad4e94ca8144e9619da76d262d24c83401c489c
Ryan <ryanjchen2@gmail.com>
2019-05-25 21:57:27 +0800

    this is cool

--- client/src/Table.js
+++ client/src/Table.js
@@ -4,7 +4,10 @@ import ReactTable from "react-table";
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-const columns = [
+import NativeSelect from '@material-ui/core/NativeSelect';
+import Input from '@material-ui/core/Input';
+
+const cols19 = [
 	{ accessor: 'name', Header: 'Name', resizable:true },
 	{ accessor: 'semester_gpa', Header: 'Semester GPA' },
 	{ accessor: 'cum_gpa', Header: 'Cumulative GPA' },
@@ -20,37 +23,105 @@ const columns = [
 	{ accessor: 'conduct', Header: 'Conduct Level' }
 ];
 
+const columns = [
+	{ accessor: 'name', Header: 'Name', resizable:true },
+	{ accessor: 'semester_gpa', Header: 'Semester GPA' },
+	{ accessor: 'cum_gpa', Header: 'Cumulative GPA' },
+	// { accessor: 'rank', Header: 'Rank' },
+	{ accessor: 'nm_gpa', Header: 'New Member GPA' },
+	{ accessor: 'new_mem', Header: 'New Members' },
+	{ accessor: 'total_mem', Header: 'Total Members' },
+	{ accessor: 'service_hrs', Header: 'Service Hours' },
+	{ accessor: 'phil', Header: 'Philantropy Money' }
+];
+
 class Table extends React.Component{
 
+	handleChange = id => event => {
+		this.setState({ [id]: event.target.value });
+	};
+
+	state = {
+		id: "f19"
+	}
+
 	render(){
-		let fall19 = require("./fall19.json");
-		return (
-		<div style={{ padding: 50 }}>
-				<Grid
-					container
-					spacing={24}
-					alignItems="flex-start"
-					justify="center"
-				>
-					<Grid item lg={10} md={4} s={6} xs={12} my={4} >
-						<Typography variant="h4">
-							pu fscl 2019 fall report
-						</Typography>
-						<Typography variant="p">
-							below you will find data reported by the 2019 purdue fscl community to purdue frat, sorority and cooperative life. we have also added our own ranking (on the way). enjoy the data as you will
-						</Typography>
-					</Grid>
-					<Grid item lg={10} md={4} s={6} xs={12} my={4} >
-								<ReactTable
-									data={fall19}
-									columns={columns}
-									defaultPageSize={10}
-									className="-striped -highlight"
-									/>
-					</Grid>
-				</Grid>	
-		</div>
-		);
+		if(this.state.id === "f19"){
+			let fall19 = require("./fall19.json");
+			return (
+			<div style={{ padding: 50 }}>
+					<Grid
+						container
+						spacing={24}
+						alignItems="flex-start"
+						justify="center"
+					>
+						<Grid item lg={10} md={4} s={6} xs={12} my={4} >
+							<Typography variant="h4">
+								pu fscl {fall19["name"]} report
+							</Typography>
+							<Typography variant="p">
+								below you will find data reported by the purdue fscl community to purdue frat, sorority and cooperative life. we have also added our own ranking (on the way). enjoy the data as you will
+							</Typography>
+							<NativeSelect
+							value={this.state.id}
+							onChange={this.handleChange("id")}
+							input={<Input name="age" id="age-native-helper" />}
+							>
+							<option value={"f19"}>Fall 2018</option>
+							<option value={"s18"}>Spring 2018</option>
+							</NativeSelect>
+						</Grid>
+						<Grid item lg={10} md={4} s={6} xs={12} my={4} >
+									<ReactTable
+										data={fall19["data"]}
+										columns={cols19}
+										defaultPageSize={10}
+										className="-striped -highlight"
+										/>
+						</Grid>
+					</Grid>	
+			</div>
+			);
+		}else{
+			let data = require("./spring18.json");
+			return (
+			<div style={{ padding: 50 }}>
+					<Grid
+						container
+						spacing={24}
+						alignItems="flex-start"
+						justify="center"
+					>
+						<Grid item lg={10} md={4} s={6} xs={12} my={4} >
+							<Typography variant="h4">
+								pu fscl {data["name"]} report
+							</Typography>
+							<Typography variant="p">
+								below you will find data reported by the purdue fscl community to purdue frat, sorority and cooperative life. we have also added our own ranking (on the way). enjoy the data as you will
+							</Typography>
+							
+							<NativeSelect
+							value={this.state.id}
+							onChange={this.handleChange("id")}
+							input={<Input name="age" id="age-native-helper" />}
+							>
+							<option value={"f19"}>Fall 2018</option>
+							<option value={"s18"}>Spring 2018</option>
+							</NativeSelect>
+						</Grid>
+						<Grid item lg={10} md={4} s={6} xs={12} my={4} >
+									<ReactTable
+										data={data["data"]}
+										columns={columns}
+										defaultPageSize={10}
+										className="-striped -highlight"
+										/>
+						</Grid>
+					</Grid>	
+			</div>
+			);
+		}
 	}
 }
 
--- helper_scripts/sheet2json.py
+++ helper_scripts/sheet2json.py
@@ -1,15 +1,15 @@
 from openpyxl import load_workbook
 import json
-wb = load_workbook(filename = '2019GreekDataPurdue.xlsx')
+wb = load_workbook(filename = '2018sFSCL.xlsx')
 sheet_ranges = wb['Overall Rankings']
 
 list = []
 
-for row in sheet_ranges.iter_rows(min_row=2, min_col=1, max_row=66, max_col=13):
+for row in sheet_ranges.iter_rows(min_row=2, min_col=1, max_row=90, max_col=9):
 	a = []
 	for cell in row:
 		a.append(cell.value)
-	list.append({"name": a[0],"semester_gpa": a[1],"cum_gpa": a[2],"rank": a[3],"nm_gpa": a[4],"new_mem": a[5],"total_mem": a[6],"service_hrs": a[7],"phil": a[8],"alc": a[9],"reporting": a[10],"attendance": a[11],"conduct": a[12]})
+	list.append({"name": a[0],"semester_gpa": a[1],"cum_gpa": a[2],"rank": a[3],"nm_gpa": a[4],"new_mem": a[5],"total_mem": a[6],"service_hrs": a[7],"phil": a[8]})
 text_file = open("Output.txt", "w")
 text_file.write(json.dumps(list))
 text_file.close()
